e21d3639cefd5f451bf9ad23bc2f48f4972b2480
ihachani <ihachaniuploads@gmail.com>
2014-03-01 09:05:10 +0100

    Tested php Dom

--- WebTechnology/WebContent/test.php
+++ WebTechnology/WebContent/test.php
@@ -8,7 +8,42 @@
 </head>
 <body>
 	<?php
-		echo "Hello , world!";
+		/*$xml=simplexml_load_file("xmls/Product/Homme/produitHomme.xml");
+		print_r($xml);
+		
+		foreach($xml->produit->children() as $child)
+  		{
+  			echo $child->getName() . ": " . $child . "<br>";
+  		}
+
+		echo $xml->produit->id . "<br>";
+		echo $xml->produit->nom . "<br>";
+		echo $xml->produit->image . "<br>";
+		echo $xml->produit->prix . "<br>";
+		echo $xml->produit->categorie;
+		*/
+		$xmlDoc = new DOMDocument();
+		$xmlDoc->load("xmls/Product/Homme/produitHomme.xml");
+
+		//print $xmlDoc->saveXML();
+
+		  $produits = $xmlDoc->getElementsByTagName( "produit" );
+  			foreach( $produits as $produit )
+  			{
+  				$id = $produit->getElementsByTagName( "id" );
+  				$id = $id->item(0)->nodeValue;
+ 
+  				$categorie = $produit->getElementsByTagName( "categorie" );
+  				$categorie = $categorie->item(0)->nodeValue;
+  
+			 	echo "$id - $categorie \n";
+  			}
+		/*$arr = array();
+		$arr[0]=0;
+		echo "$arr[0]";*/
+		/*function RechercheProductNom($xmlDoc,$string){
+			retVal = 
+		}*/
 	?>
 </body>
 </html>
