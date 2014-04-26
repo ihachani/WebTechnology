0d75c866b0ec7e5320626a0565c44bf1a92648c2
ihachani <ihachaniuploads@gmail.com>
2014-04-26 15:38:10 +0100

    Add product %100
    Signed-off-by: ihachani <ihachaniuploads@gmail.com>

--- WebContent/js/admin.js
+++ WebContent/js/admin.js
@@ -2,26 +2,6 @@
  * 
  */
 
-function createSelectFour() {
-	$.ajax({
-		url : 'fournisseur.php',
-		type : 'post',
-		data : {
-			'action' : 'showfourn',
-		},
-		dataType : 'json',
-		success : function(data) {
-			$.each(data, function(index, value) {
-				alert(value.nom);
-			});
-		},
-		error : function(xhr, desc, err) {
-			console.log(xhr);
-			console.log("Details: " + desc + "\nError:" + err);
-		}
-	});
-}
-
 function addFour() {
 	$.ajax({
 		url : 'fournisseur.php',
@@ -41,6 +21,56 @@ function addFour() {
 	});
 }
 
+function addProdphp() {
+	$.ajax({
+		url : 'produit.php',
+		type : 'post',
+		data : {
+			'action' : 'addProd',
+			'id' : $('#prodID').val(),
+			'nom' : $('#prodName').val(),
+			'prix' : $('#prodPrix').val(),
+			'quantite' : 0,
+			'category' : $("#prodCat").val(),
+			'fournisseur' : $("#prodFourn option:selected").text(),
+			'image' : 'assets/img.png'
+		},
+		dataType : 'text',
+		success : function(data) {
+			alert(data);
+		},
+		error : function(xhr, desc, err) {
+			console.log(xhr);
+			console.log("Details: " + desc + "\nError:" + err);
+		}
+	});
+}
+
+function addProd() {
+	if ($("#prodPrix").val() && $("#prodName").val() && $("#prodID").val()) {
+		if ($("#prodName").val().indexOf('<') != -1
+				|| $("#prodName").val().indexOf('>') != -1
+				|| $("#prodID").val().indexOf('<') != -1
+				|| $("#prodID").val().indexOf('>') != -1)
+			alert('Character invalide');
+		else {
+			if (isNaN(val = $("#prodPrix").val())) {
+				alert('not a number');
+			} else {
+				if (val < 0) {
+					alert('negative number');
+				} else {
+					addProdphp();
+				}
+
+			}
+		}
+
+	} else
+		alert('a field is empty');
+
+}
 $(function() {
 	$(document).on('click', '#fourSubmit', addFour);
+	$(document).on('click', '#prodSubmit', addProd);
 });
---
+++ WebContent/produit.php
@@ -0,0 +1,34 @@
+<?php
+include_once 'xmlFile.php';
+$fields = array ();
+$fields [] = 'produits';
+$fields [] = 'produit';
+$fields [] = 'id';
+$fields [] = 'nom';
+$fields [] = 'prix';
+$fields [] = 'quantite';
+$fields [] = 'category';
+$fields [] = 'fournisseur';
+$fields [] = 'image';
+
+$obj = new xmlFile ( 'xmls/produits.xml', $fields, 9, 2 );
+
+if ($_POST ['action'] == 'addProd') {
+	$values = array ();
+	$values [] = '';
+	$values [] = '';
+	$values [] = $_POST ['id'];
+	$values [] = $_POST ['nom'];
+	$values [] = $_POST ['prix'];
+	$values [] = $_POST ['quantite'];
+	$values [] = $_POST ['category'];
+	$values [] = $_POST ['fournisseur'];
+	$values [] = $_POST ['image'];
+	
+	$status = $obj->addToXml ( $values );
+	if ($status == false)
+		echo "Problem lors de l'ajout de " . $_POST ['id'];
+	else
+		echo $_POST ['id'] . " ajouté avec success";
+}
+?>
