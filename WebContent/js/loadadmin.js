64ea7796d09efa1f71e08f64b60e9a0eac1019dd
ihachani <ihachaniuploads@gmail.com>
2014-04-26 15:38:10 +0100

    Add product %100
    Signed-off-by: ihachani <ihachaniuploads@gmail.com>

--- WebContent/js/loadadmin.js
+++ WebContent/js/loadadmin.js
@@ -2,12 +2,33 @@
  * 
  */
 
+function createSelectFour() {
+	$.ajax({
+		url : 'fournisseur.php',
+		type : 'post',
+		data : {
+			'action' : 'showfourn',
+		},
+		dataType : 'json',
+		success : function(data) {
+			$.each(data, function(key, value) {
+				$('#prodFourn').append(
+						$("<option></option>").attr("value", value.nom).text(
+								value.nom));
+			});
+		},
+		error : function(xhr, desc, err) {
+			console.log(xhr);
+			console.log("Details: " + desc + "\nError:" + err);
+		}
+	});
+}
 function loadFournAdd() {
 	$("#container").load("fournisseur.html");
 }
 
 function loadProdAdd() {
-	$("#container").load("produit.html");
+	$("#container").load("produit.html", createSelectFour);
 }
 $(function() {
 	$(document).on('click', '#fournisseur', loadFournAdd);
