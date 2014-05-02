c64cbcaf2a0607a2b35fd18908a20066a8112452
ihachani <ihachaniuploads@gmail.com>
2014-05-02 19:21:28 +0100

    Adding products to test out.
    Signed-off-by: ihachani <ihachaniuploads@gmail.com>

---
+++ WebContent/js/imgdiv.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+function createImageDiv(item) {
+	retval = $('<div>', {
+		class : 'imgdiv',
+	}).css('background-image', 'url(' + item.image + ')');
+	toolbar = $('<div>', {
+		class : 'toolbar'
+	});
+	toolbar.append($('<p>').text(item.nom));
+	price = $('<p>', {
+		class : 'price'
+	});
+	price.html('<span>' + item.prix + '</span> DT');
+	toolbar.append(price);
+	toolbar.append($('<button>', {
+		target : item.id
+	}).text('acheter'));
+	filtre = $('<div>', {
+		class : 'filtre'
+	});
+	filtre.append(toolbar);
+	retval.append(filtre);
+	return retval;
+}
+function imageDivFiltre() {
+	$(document).on('mouseenter', '.imgdiv', function() {
+		$('.filtre', this).show(200);
+	});
+	$(document).on('mouseleave', '.imgdiv', function() {
+		$('.filtre', this).hide(100);
+	});
+}
+function showAll() {
+	$.ajax({
+		url : 'produit.php',
+		type : 'post',
+		data : {
+			'action' : 'showprod',
+		},
+		dataType : 'json',
+		success : function(data) {
+			$.each(data, function(key, value) {
+				$('#main_view').append(createImageDiv(value));
+			});
+		},
+		error : function(xhr, desc, err) {
+			console.log(xhr);
+			console.log("Details: " + desc + "\nError:" + err);
+		}
+	});
+}
+$(function() {
+	imageDivFiltre();
+	showAll();
+});
